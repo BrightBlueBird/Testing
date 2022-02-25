f317eec5ee22337c2fca5743e98176cb3cc4339b
BrightBlueBird <99009695+BrightBlueBird@users.noreply.github.com>
2022-02-25 09:21:02 +0100

    Added greet

--- src/gitSomething.java
+++ src/gitSomething.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class gitSomething {
+  void greet() {
+    Scanner scan = new Scanner(System.in);
+    String name;
+    System.out.println("Please enter your name");
+    name = scan.nextLine();
+    System.out.println("Your name is: " + name);
+
+  }
   void go () {
     System.out.println("It works");
+    greet();
   }
   public static void main(String[] args) {
     new gitSomething().go();
