b2f9b4ecb89eee673cee5404275d1c7b3d2ec14c
BrightBlueBird <99009695+BrightBlueBird@users.noreply.github.com>
2022-02-25 09:27:46 +0100

    Added askAboutAge

--- src/gitSomething.java
+++ src/gitSomething.java
@@ -1,17 +1,25 @@
 import java.util.Scanner;
 
 public class gitSomething {
+  Scanner scan = new Scanner(System.in);
+  void askAboutAge() {
+    int age;
+    System.out.println("Please enter your age");
+    age = scan.nextInt();
+    System.out.println("You are: " + age + " years old.");
+  }
   void greet() {
     Scanner scan = new Scanner(System.in);
     String name;
     System.out.println("Please enter your name");
     name = scan.nextLine();
-    System.out.println("Your name is: " + name);
+    System.out.println("Hej: " + name);
 
   }
   void go () {
     System.out.println("It works");
     greet();
+    askAboutAge();
   }
   public static void main(String[] args) {
     new gitSomething().go();
